9e2ab98b5c23949de71d6251df6849df1aad1c77
Punnarak <Pun_zaa_ii@hotmail.com>
2023-04-04 15:23:31 +0700

    add file

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -12,18 +11,14 @@ import {
   TouchableOpacity,
   TextInput,
 } from "react-native";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import TextStroke from "react-native-textstroke";
-// import { MaterialCommunityIcons } from "@expo/vector-icons";
-// import { useTogglePasswordVisibility } from "./useTogglePasswordVisibility";
+
+import { Feather } from "@expo/vector-icons";
+import usePasswordVisibility from "./usePasswordVisibility";
 
 //View -> UIView
 export default function App() {
-  // const { passwordVisibility, rightIcon, handlePasswordVisibility } =
-  //   useTogglePasswordVisibility();
-  // const [password, setPassword] = useState("");
-
+  const { passwordVisibility, togglePasswordVisibility } =
+    usePasswordVisibility();
   console.log("App executed!");
   return (
     <View style={styles.container}>
@@ -35,19 +30,23 @@ export default function App() {
       />
 
       <Text style={styles.title1}>Email</Text>
-      <TextInput
-        style={styles.TextInput} /*placeholder="type your username"*/
-      />
+      <TextInput style={styles.TextInput} />
 
       <Text style={styles.title2}>Password</Text>
+
       <TextInput
-        secureTextEntry={true}
-        style={styles.TextInput} /*placeholder="type your password" */
+        secureTextEntry={passwordVisibility}
+        style={styles.TextInput}
       />
-      {/* <Pressable onPress={handlePasswordVisibility}>
-          <MaterialCommunityIcons name={rightIcon} size={22} color="#232323" />
-        </Pressable>
-      </TextInput> */}
+
+      <TouchableOpacity style={styles.eyeI} onPress={togglePasswordVisibility}>
+        <Feather
+          name={passwordVisibility ? "eye-off" : "eye"}
+          size={20}
+          color="black"
+        />
+      </TouchableOpacity>
+
       <Text
         style={styles.hyper}
         onPress={() => Linking.openURL("http://google.com")}
@@ -75,28 +74,9 @@ export default function App() {
       <View style={styles.undertag}>
         <Text style={styles.tu}>Sa-Mind</Text>
       </View>
-
-      {/* <Button title="Login" onPress={() => alert("Button tapped")} /> */}
-      {/* <Button
-        title="Login"
-        onPress={() =>
-          Alert.alert("My title", "My message", [
-            { text: "Yes", onPress: () => console.log("Yes") },
-            { text: "No", onPress: () => console.log("No") },
-          ])
-        }
-      /> */}
-      {/* <Button
-        title="Login"
-        onPress={() =>
-          Alert.prompt("My title", "My message", (text) => console.log(text))
-        }
-      /> */}
-      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -123,6 +103,13 @@ const styles = StyleSheet.create({
     marginTop: "4%",
     marginRight: "62%",
     fontWeight: "bold",
+  },
+
+  eyeI: {
+    flex: 1,
+    alignItems: "flex-end",
+    marginTop: "132%",
+    position: "absolute",
   },
   hyper: {
     fontSize: 10,
@@ -181,177 +168,3 @@ const styles = StyleSheet.create({
     color: "rgb(238,186,0)",
   },
 });
-
-// // import * as React from "react";
-// // import { NavigationContainer } from "@react-navigation/native";
-// // import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-// // const Stack = createNativeStackNavigator();
-
-// // const MyStack = () => {
-// //   return (
-// //     <NavigationContainer>
-// //       <Stack.Navigator>
-// //         <Stack.Screen
-// //           name="Home"
-// //           component={HomeScreen}
-// //           options={{ title: "Welcome" }}
-// //         />
-// //         <Stack.Screen name="Profile" component={ProfileScreen} />
-// //       </Stack.Navigator>
-// //     </NavigationContainer>
-// //   );
-// // };
-// // export default App;
-// // const HomeScreen = ({ navigation }) => {
-// //   return (
-// //     <Button
-// //       color="pink"
-// //       title="Go to Jane's profile"
-// //       onPress={() => navigation.navigate("Profile", { name: "Jane" })}
-// //     />
-// //   );
-// // };
-// // const ProfileScreen = ({ navigation, route }) => {
-// //   return <Text>This is {route.params.name}'s profile</Text>;
-// // };
-
-// // import React from "react";
-// // import { View, Text, Button } from "react-native";
-// // import { createStackNavigator, createAppContainer } from "react-navigation";
-
-// // class HomeScreen extends React.Component {
-// //   render() {
-// //     return (
-// //       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-// //         <Text>Home Screen</Text>
-// //         <Button
-// //           title="Go to Profile"
-// //           onPress={() => this.props.navigation.navigate("Profile")}
-// //         />
-// //       </View>
-// //     );
-// //   }
-// // }
-// // class ProfileScreen extends React.Component {
-// //   render() {
-// //     return (
-// //       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-// //         <Text>Profile Screen</Text>
-// //       </View>
-// //     );
-// //   }
-// // }
-
-// // const AppNavigator = createStackNavigator(
-// //   {
-// //     Home: HomeScreen,
-// //     Profile: ProfileScreen,
-// //   },
-// //   {
-// //     initialRouteName: "Home",
-// //   }
-// // );
-
-// // const AppContainer = createAppContainer(AppNavigator);
-// // export default class App extends React.Component {
-// //   render() {
-// //     return <AppContainer />;
-// //   }
-// // }
-// // import React, { useState } from "react";
-// // import { StyleSheet, Pressable, TextInput, View } from "react-native";
-// // import { MaterialCommunityIcons } from "@expo/vector-icons";
-
-// // import { useTogglePasswordVisibility } from "./useTogglePasswordVisibility";
-
-// // export default function App() {
-// //   const { passwordVisibility, rightIcon, handlePasswordVisibility } =
-// //     useTogglePasswordVisibility();
-// //   const [password, setPassword] = useState("");
-
-// //   return (
-// //     <View style={styles.container}>
-// //       <View style={styles.inputContainer}>
-// //         <TextInput
-// //           style={styles.inputField}
-// //           name="password"
-// //           placeholder="Enter password"
-// //           autoCapitalize="none"
-// //           autoCorrect={false}
-// //           textContentType="newPassword"
-// //           secureTextEntry={passwordVisibility}
-// //           value={password}
-// //           enablesReturnKeyAutomatically
-// //           onChangeText={(text) => setPassword(text)}
-// //         />
-// //         <Pressable onPress={handlePasswordVisibility}>
-// //           <MaterialCommunityIcons name={rightIcon} size={22} color="white" />
-// //         </Pressable>
-// //       </View>
-// //     </View>
-// //   );
-// // }
-
-// // const styles = StyleSheet.create({
-// //   container: {
-// //     flex: 1,
-// //     backgroundColor: "#F5EEDC",
-// //     alignItems: "center",
-// //     justifyContent: "center",
-// //     paddingHorizontal: 12,
-// //   },
-// //   inputContainer: {
-// //     backgroundColor: "white",
-// //     width: "100%",
-// //     borderRadius: 8,
-// //     flexDirection: "row",
-// //     alignItems: "center",
-// //     borderWidth: 4,
-// //     borderColor: "#d7d7d7",
-// //   },
-// //   inputField: {
-// //     padding: 14,
-// //     fontSize: 22,
-// //     width: "90%",
-// //   },
-// // });
-// import * as React from "react";
-// import { Button, View, Text } from "react-native";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-// function HomeScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//       <Text>Home Screen</Text>
-//       <Button
-//         title="Go to Details"
-//         onPress={() => navigation.navigate("Details")}
-//       />
-//     </View>
-//   );
-// }
-
-// function DetailsScreen() {
-//   return (
-//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//       <Text>Details Screen</Text>
-//     </View>
-//   );
-// }
-
-// const Stack = createNativeStackNavigator();
-
-// function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator initialRouteName="Home">
-//         <Stack.Screen name="Home" component={HomeScreen} />
-//         <Stack.Screen name="Details" component={DetailsScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-// export default App;
