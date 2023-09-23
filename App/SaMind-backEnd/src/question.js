612af991e539ed4c803c0a01c2a262cb243a5fd2
Jare-u <72984248+Jare-u@users.noreply.github.com>
2023-09-23 23:40:32 +0700

    Update question.js

--- App/SaMind-backEnd/src/question.js
+++ App/SaMind-backEnd/src/question.js
@@ -2,9 +2,30 @@ const client = require('./connection.js')
 const express = require('express');
 const router = express.Router();
 
+// router.get('/question', (req, res) => {
+//   const type = req.query.type; // Get the type parameter from the query
+//   let query = 'SELECT id, question, options FROM questionnaire';
+
+//   // Check if the type parameter is provided
+//   if (type) {
+//     query += ' WHERE type LIKE $1';
+//   }
+
+//   const queryParams = type ? [`%${type}%`] : [];
+
+//   client.query(query, queryParams)
+//     .then(result => {
+//       res.json(result.rows);
+//     })
+//     .catch(err => {
+//       console.error('Error executing query:', err);
+//       res.status(500).json({ error: 'An error occurred' });
+//     });
+// });
+
 router.get('/question', (req, res) => {
   const type = req.query.type; // Get the type parameter from the query
-  let query = 'SELECT * FROM questionnaire';
+  let query = 'SELECT id, question, options FROM questionnaire';
 
   // Check if the type parameter is provided
   if (type) {
@@ -15,7 +36,25 @@ router.get('/question', (req, res) => {
 
   client.query(query, queryParams)
     .then(result => {
-      res.json(result.rows);
+      const formattedResults = result.rows.map(row => {
+        let options;
+
+        try {
+          // Attempt to parse the options field as JSON
+          options = JSON.parse(row.options);
+        } catch (error) {
+          // Handle any parsing errors, e.g., if the data is not valid JSON
+          options = null; // Or handle the error in an appropriate way for your use case
+        }
+
+        return {
+          id: row.id,
+          question: row.question,
+          options: options
+        };
+      });
+
+      res.json(formattedResults);
     })
     .catch(err => {
       console.error('Error executing query:', err);
@@ -23,16 +62,38 @@ router.get('/question', (req, res) => {
     });
 });
 
-router.post('/questionAdd', (req, res) => {
-  const { questionid, question, choice, type } = req.body;
 
-  if (!question || !questionid || !type || !choice ) {
+// router.post('/questionAdd', (req, res) => {
+//   const { id, question, options, type } = req.body;
+
+//   if (!id || !question || !options || !type ) {
+//     return res.status(400).json({ error: 'Both question and choice are required fields.' });
+//   }
+
+//   const insertQuery = 'INSERT INTO questionnaire (id, question, options, type) VALUES ($1, $2, $3, $4) RETURNING *';
+
+//   client.query(insertQuery, [id, question, options, type])
+//     .then(result => {
+//       res.status(201).json(result.rows[0]);
+//     })
+//     .catch(err => {
+//       console.error('Error executing query:', err);
+//       res.status(500).json({ error: 'An error occurred' });
+//     });
+// });
+
+router.post('/questionAdd', (req, res) => {
+  const { id, question, options, type } = req.body;
+
+  if (!id || !question || !options || !type) {
     return res.status(400).json({ error: 'Both question and choice are required fields.' });
   }
 
-  const insertQuery = 'INSERT INTO questionnaire (questionid, question, choice, type) VALUES ($1, $2, $3, $4) RETURNING *';
+  const optionsString = JSON.stringify(options); // Convert options array to JSON string
 
-  client.query(insertQuery, [questionid, question, choice, type])
+  const insertQuery = 'INSERT INTO questionnaire (id, question, options, type) VALUES ($1, $2, $3, $4) RETURNING *';
+
+  client.query(insertQuery, [id, question, optionsString, type])
     .then(result => {
       res.status(201).json(result.rows[0]);
     })
@@ -41,5 +102,6 @@ router.post('/questionAdd', (req, res) => {
       res.status(500).json({ error: 'An error occurred' });
     });
 });
+
 
 module.exports = router;
