8dc25877e91f407d47c6de4a482672170c39986f
Jare-u <72984248+Jare-u@users.noreply.github.com>
2023-09-27 21:35:31 +0700

    Update dashboard.js

--- App/SaMind-backEnd/src/dashboard.js
+++ App/SaMind-backEnd/src/dashboard.js
@@ -1,19 +1,83 @@
-const client = require('./connection.js')
+const client = require('./connection.js');
+// const avatar = require('./connection.js');
+// const { client, avatar, game } = require('./connection.js');
+
 const express = require('express');
 const router = express.Router();
 
-router.get('/dashboard', (req, res) => {
-    const id = req.query.patientID;
-    const query = 'SELECT * FROM dashboard WHERE patientID';
+
+router.get('/dash_AVG_mood', (req, res) => {
+  const id = req.query.id; // Get the id parameter from the query
+  let query = 'SELECT * FROM mood_tracker';
+
+  // Check if the id parameter is provided
+  if (id) {
+    query += ' WHERE id = $1';
+  }
+
+  // Add an "ORDER BY" clause to sort the result by the "id" column
+  query += ' ORDER BY id';
+
+  const queryParams = id ? [id] : [];
   
-    client.query(query, [`%${id}%`])
-      .then(result => {
-        res.json(result.rows);
-      })
-      .catch(err => {
-        console.error('Error executing query:', err);
-        res.status(500).json({ error: 'An error occurred' });
-      });
+
+  client.query(query, queryParams)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'An error occurred' });
+    });
 });
 
+router.get('/dashTest', (req, res) => {
+  const id = req.query.id; // Get the id parameter from the query
+  let query = 'SELECT * FROM test_score';
+
+  // Check if the id parameter is provided
+  if (id) {
+    query += ' WHERE id = $1';
+  }
+
+  // Add an "ORDER BY" clause to sort the result by the "id" column
+  query += ' ORDER BY id';
+
+  const queryParams = id ? [id] : [];
+
+  client.query(query, queryParams)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'An error occurred' });
+    });
+});
+
+
+// router.get('/dash_mood_detection', (req, res) => {
+//   const id = req.query.id; // Get the id parameter from the query
+//   let query = 'SELECT * FROM mood_detection';
+
+//   // Check if the id parameter is provided
+//   if (id) {
+//     query += ' WHERE id = $1';
+//   }
+
+//   // Add an "ORDER BY" clause to sort the result by the "id" column
+//   query += ' ORDER BY id';
+
+//   const queryParams = id ? [id] : [];
+
+//   avatar.query(query, queryParams)
+//     .then(result => {
+//       res.json(result.rows);
+//     })
+//     .catch(err => {
+//       console.error('Error executing query:', err);
+//       res.status(500).json({ error: 'An error occurred' });
+//     });
+// });
+  
 module.exports = router;
