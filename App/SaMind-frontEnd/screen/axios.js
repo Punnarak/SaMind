43b9c5bd76eb018645b8bc25302b12de6a3b785b
Jare-u <72984248+Jare-u@users.noreply.github.com>
2023-12-09 09:51:51 +0700

    update axios

--- App/SaMind-frontEnd/screen/axios.js
+++ App/SaMind-frontEnd/screen/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const HTTP = axios.create({
   baseURL: "http://192.168.56.1:4343",
+  // baseURL: "https://ba0c-49-229-132-211.ngrok-free.app",
 });
 export default HTTP;
