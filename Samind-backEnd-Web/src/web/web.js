bbcb33c5a905d693430b0a790669d28843100b4f
Jare-u <72984248+Jare-u@users.noreply.github.com>
2024-02-08 20:45:49 +0700

    update login web

--- Samind-backEnd-Web/src/web/web.js
+++ Samind-backEnd-Web/src/web/web.js
@@ -2,17 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const web = express();
 const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
 
 web.use(cors());
 web.use(bodyParser.json());
 web.use(bodyParser.urlencoded({ extended: true }));
+web.use(cookieParser());
 
 // Import API file
 const patientList = require('./patientList.js');
 const allTest = require('./test.js');
-const info_therapist = require('./info_user.js')
-const calendar_view = require('./calendar.js')
-const appointment = require('./appointment.js')
+const info_therapist = require('./info_user.js');
+const calendar_view = require('./calendar.js');
+const appointment = require('./appointment.js');
+const login = require ('./login.js');
 
 // Use the API file
 web.use(patientList);
@@ -20,6 +23,7 @@ web.use(allTest);
 web.use(info_therapist);
 web.use(calendar_view);
 web.use(appointment);
+web.use(login);
 
 web.listen(4301, function () {
   console.log('CORS-enabled web server listening on port 4301');
