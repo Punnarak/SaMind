9bc8f716bfd4bfe33bb2f2d06c76392ec1158b45
Jare-u <72984248+Jare-u@users.noreply.github.com>
2024-02-19 20:33:23 +0700

    axios

--- Samind_web/src/axios.js
+++ Samind_web/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const HTTP = axios.create({
   // baseURL: "https://dfc4-2001-fb1-15-30ce-cc3a-8c0d-162c-cc5b.ngrok-free.app",
-  baseURL: "http://10.226.165.184:4301",
+  baseURL: "http://10.226.167.127:4301",
 });
 export default HTTP;
