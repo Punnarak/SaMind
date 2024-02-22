a588b01b4c7836b35ce7211b038c969c04cf212d
Punnarak <50368218+Punnarak@users.noreply.github.com>
2024-02-22 23:33:54 +0700

    request bar -> box for new appointment

--- Samind_web/src/views/Calendar.vue
+++ Samind_web/src/views/Calendar.vue
@@ -13,7 +13,7 @@
             style="width: auto"
           >
             <row>
-              <label class="title ml-5"> POSTPONE MEETING REQUEST </label>
+              <label class="title ml-5"> APPOINTMENT REQUEST </label>
               <v-btn icon variant="text" @click="toggleRequestBar">
                 <v-icon>mdi-close</v-icon>
               </v-btn></row
@@ -27,9 +27,114 @@
                     'confirmed-item': patient.confirmed,
                     'cancel-item': patient.cancel,
                     'sure-item': patient.sureConfirmed || patient.sureCancel,
+                    'new-appointment': patient.new,
                   }"
                   class="custom-chip mb-5"
                 >
+                  <div
+                    v-if="
+                      !patient.confirmed &&
+                      !patient.cancel &&
+                      !patient.sureCancel &&
+                      !patient.sureConfirmed &&
+                      patient.toDate === '-' &&
+                      patient.toTime === '-'
+                    "
+                  >
+                    <div style="margin-top: -10px" v-if="patient.value == ''">
+                      <div
+                        style="
+                          display: flex;
+                          align-items: center;
+                          margin-top: -40px;
+                          position: absolute;
+                        "
+                      >
+                        <div
+                          class="circle mr-2"
+                          style="
+                            background-color: rgba(86, 154, 255, 1);
+                            width: 30px;
+                            height: 30px;
+                          "
+                        >
+                          <v-icon style="color: white">mdi-account</v-icon>
+                        </div>
+
+                        <label class="nametext">{{
+                          patient.patientName
+                        }}</label>
+                      </div>
+                      <br />
+                      <div>
+                        <div class="circle-container">
+                          <div
+                            class="circle"
+                            style="
+                              background-color: rgba(0, 191, 99, 1);
+                              margin-bottom: 35px;
+                            "
+                          ></div>
+                        </div>
+
+                        <div
+                          style="
+                            display: flex;
+                            position: absolute;
+                            margin-top: -50px;
+                            margin-left: 70px;
+                          "
+                        >
+                          <label
+                            class="text mr-8"
+                            style="color: rgba(0, 191, 99, 1); font-size: 13px;'"
+                            >Request</label
+                          >
+                          <label
+                            class="text"
+                            style="
+                              color: rgba(0, 191, 99, 1);
+                              font-weight: 500;
+                              font-size: 13px;
+                              text-align: start;
+                            "
+                            >{{ patient.toDate }}<br />{{ patient.toTime }}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      style="
+                        display: flex;
+                        margin-top: 30px;
+                        margin-left: 5px;
+                        position: absolute;
+                      "
+                    >
+                      <v-btn
+                        class="requestbtn mr-2"
+                        style="
+                          color: rgba(0, 191, 99, 1);
+                          border: 1px solid #00bf63;
+                          box-shadow: 0px 0px 2px 0px #00bf63;
+                        "
+                        @click="confirm(patientIndex)"
+                      >
+                        confirm
+                      </v-btn>
+                      <v-btn
+                        class="requestbtn"
+                        style="
+                          color: rgba(242, 86, 86, 1);
+                          border: 1px solid rgba(242, 86, 86, 1);
+                          box-shadow: 0px 0px 2px 0px rgba(242, 86, 86, 1);
+                        "
+                        @click="cancel(patientIndex)"
+                      >
+                        cancel
+                      </v-btn>
+                    </div>
+                  </div>
                   <div v-if="patient.confirmed" class="confirmtext">
                     <label>
                       Are you sure you want to
@@ -115,7 +220,9 @@
                       !patient.confirmed &&
                       !patient.cancel &&
                       !patient.sureCancel &&
-                      !patient.sureConfirmed
+                      !patient.sureConfirmed &&
+                      patient.toDate !== '-' &&
+                      patient.toTime !== '-'
                     "
                   >
                     <div style="margin-top: -10px" v-if="patient.value == ''">
@@ -557,6 +664,16 @@ export default {
           patientName: "Somsom Test4",
           toDate: "Tue, 19 Sep 2023",
           toTime: "10.00",
+          fromDate: "Tue, 12 Sep 2023",
+          fromTime: "10.00",
+          value: "",
+        },
+        {
+          id: "05",
+          patientId: "PID004",
+          patientName: "Somsom Test4",
+          toDate: "-",
+          toTime: "-",
           fromDate: "Tue, 12 Sep 2023",
           fromTime: "10.00",
           value: "",
