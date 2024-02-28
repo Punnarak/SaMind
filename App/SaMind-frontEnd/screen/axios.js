cabbf77729af7c404063af4eef9010886e7589e2
Punnarak <50368218+Punnarak@users.noreply.github.com>
2024-02-28 18:11:04 +0700

    commit axios

--- App/SaMind-frontEnd/screen/axios.js
+++ App/SaMind-frontEnd/screen/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const HTTP = axios.create({
-  baseURL: "http://192.168.1.37:4343",
-  // baseURL: "https://71ce-49-229-135-252.ngrok-free.app",
+  // baseURL: "http://192.168.1.37:4343",
+  baseURL: "https://6ef0-49-229-130-189.ngrok-free.app",
 });
 export default HTTP;
 
