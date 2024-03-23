7655ff8b066246dba313b975205777f080b35557
Punnarak <50368218+Punnarak@users.noreply.github.com>
2024-03-24 01:14:24 +0700

    axios

--- App/SaMind-frontEnd/screen/axios.js
+++ App/SaMind-frontEnd/screen/axios.js
@@ -10,14 +10,14 @@ import axios from "axios";
 
 const HTTP = axios.create({
   // baseURL: "http://192.168.1.37:4343",
-  baseURL: "https://1ab3-49-229-129-109.ngrok-free.app",
+  baseURL: "https://a066-49-229-132-157.ngrok-free.app",
   // baseURL: "http://192.168.1.38:4343",
 });
 
-const path = HTTP.defaults.baseURL
+const path = HTTP.defaults.baseURL;
 const HTTPpython = axios.create({
   // baseURL: "http://192.168.1.37:4343",
   // baseURL: "https://6ef0-49-229-130-189.ngrok-free.app",
   baseURL: "http://192.168.1.38:5000",
 });
-export  {HTTP as axios, HTTPpython as axiospython, path};
+export { HTTP as axios, HTTPpython as axiospython, path };
