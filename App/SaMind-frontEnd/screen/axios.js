8d07f1598be605c35ee16478f867a559f03ccc30
Jare-u <72984248+Jare-u@users.noreply.github.com>
2024-05-11 02:05:46 +0700

    Update axios.js

--- App/SaMind-frontEnd/screen/axios.js
+++ App/SaMind-frontEnd/screen/axios.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 const HTTP = axios.create({
-  // baseURL: "http://192.168.1.37:4343",
+  baseURL: "http://10.226.165.4:4343",
   // baseURL: "https://6ef0-49-229-130-189.ngrok-free.app",
-  baseURL: "http://192.168.1.38:4343",
+  // baseURL: "https://52.63.236.148/api/app",
 });
 
 const path = HTTP.defaults.baseURL;
 const HTTPpython = axios.create({
   // baseURL: "http://192.168.1.37:4343",
   // baseURL: "https://6ef0-49-229-130-189.ngrok-free.app",
-  baseURL: "http://192.168.1.38:5000",
+  baseURL: "http://10.226.165.4:5000",
 });
 export { HTTP as axios, HTTPpython as axiospython, path };
